fb52d359923136f46b418625c205bddfe11f601c
Shattered217 <shattered0217@gmail.com>
2024-12-16 15:18:46 +0800

    小更改

--- _example/PC_status.py
+++ _example/PC_status.py
@@ -9,21 +9,20 @@ windows_list = []
 win32gui.EnumWindows(lambda hWnd, param: param.append(hWnd), windows_list)
 
 def get_desktop():
-    output_set.clear()  # 每次调用前清空 output_set
+    output_set.clear()
     for window in windows_list:
         title = win32gui.GetWindowText(window)
         if title in white_list and title not in output_set:
             output_set.add(title)
 
 def seed():
-    url = "https://onlsy.shattered.top/pc_status"
-    secret = "ilovesy"
+    url = "https://domain/pc_status" # 你的服务器地址
+    secret = "123456" # 你的secret
     pc_status = "0"
 
-    get_desktop()  # 获取当前进程
-    pc_app_name = list(output_set)  # 将 output_set 转换为列表
+    get_desktop()
+    pc_app_name = list(output_set)
 
-    # 打印出当前找到的程序
     print(f"Current apps: {pc_app_name}")
 
     params = {
@@ -31,11 +30,9 @@ def seed():
         "pc_status": pc_status,
     }
 
-    # 对每个应用程序，使用相同的键（pcapp_name）传递多个值
     for app in pc_app_name:
         params.setdefault("pc_app_name", []).append(app)
 
-    # 发送请求
     response = requests.get(url, params=params)
     if response.status_code == 200:
         print("信息发送成功:", response.text)
